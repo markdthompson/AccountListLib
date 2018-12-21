e97b964b243851808c55bc4bc9ba6e0f259fb520
Mark D. Thompson <thomesoni@gmail.com>
2018-12-21 11:48:19 -0500

    Update AccountListLib.sol

--- AccountListLib.sol
+++ AccountListLib.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.24;
 
 library AccountList {
     
-    function SwapSort(address[] storage _arr, uint _index) public returns(address[]){
+    function SwapSort(address[] storage _arr, uint _index) internal returns(address[]){
         _arr[_index] = _arr[_arr.length - 1];
         delete _arr[_arr.length - 1];
         _arr.length--;
@@ -10,7 +10,7 @@ library AccountList {
         return _arr;
     }
      
-    function ReorderSort(address[] storage _arr, uint _index) public returns(address[]){
+    function ReorderSort(address[] storage _arr, uint _index) internal returns(address[]){
         for(uint i = _index; i < _arr.length - 1; i++) {
             _arr[_index] = _arr[_index + 1];
         }
