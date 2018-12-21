642a06d95ec5970cd5e5a7b366aa7c379bc5793a
Mark D. Thompson <thomesoni@gmail.com>
2018-12-21 11:07:13 -0500

    Create AccountListLib.sol

---
+++ AccountListLib.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.24;
+
+library AccountListLib {
+    
+    struct AccountList {
+      address[] list; 
+    }
+    
+    function addressSwapSort(address[] storage _arr, uint _index) private returns(address[]){
+        _arr[_index] = _arr[_arr.length - 1];
+        delete _arr[_arr.length - 1];
+        _arr.length--;
+        
+        return _arr;
+    }
+     
+    function addressReorderSort(address[] storage _arr, uint _index) private returns(address[]){
+        for(uint i = _index; i < _arr.length - 1; i++) {
+            _arr[_index] = _arr[_index + 1];
+        }
+        delete _arr[_arr.length - 1];
+        _arr.length--;
+        
+        return _arr;
+    }
+}
