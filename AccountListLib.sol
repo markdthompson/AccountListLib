ab5ab308670dde66c34c9c1a7cc800910c69126f
Mark D. Thompson <thomesoni@gmail.com>
2019-01-04 19:26:57 -0500

    added storage type 'memory'

--- AccountListLib.sol
+++ AccountListLib.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 library AccountList {
     
-    function SwapSort(address[] storage _arr, uint _index) internal returns(address[]){
+    function SwapSort(address[] storage _arr, uint _index) internal returns(address[] memory){
         _arr[_index] = _arr[_arr.length - 1];
         delete _arr[_arr.length - 1];
         _arr.length--;
@@ -10,7 +10,7 @@ library AccountList {
         return _arr;
     }
      
-    function ReorderSort(address[] storage _arr, uint _index) internal returns(address[]){
+    function ReorderSort(address[] storage _arr, uint _index) internal returns(address[] memory){
         for(uint i = _index; i < _arr.length - 1; i++) {
             _arr[_index] = _arr[_index + 1];
         }
@@ -20,7 +20,7 @@ library AccountList {
         return _arr;
     }
     
-    function UintSwapSort(address[] storage _arr, uint _index) internal returns(address[]){
+    function UintSwapSort(address[] storage _arr, uint _index) internal returns(address[] memory){
         _arr[_index] = _arr[_arr.length - 1];
         delete _arr[_arr.length - 1];
         _arr.length--;
@@ -28,7 +28,7 @@ library AccountList {
         return _arr;
     }
     
-    function UintReorderSort(uint[] storage _arr, uint _index) internal returns(uint[]){
+    function UintReorderSort(uint[] storage _arr, uint _index) internal returns(uint[] memory){
         for(uint i = _index; i < _arr.length - 1; i++) {
             _arr[_index] = _arr[_index + 1];
         }
